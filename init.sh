ba9a3f40040a8bddfc61782d9b6ce69923895f03
Liam Raven <liam.raven@bbc.co.uk>
2017-02-04 20:57:42 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [ ! -f $SAVEFILE ]; then
+if [ $SAVEFILE ]; 
+then
+echo "LOAD SAVE FILE"
+else
+  echo "NO SAVE FILE CREATING SAVE FILE"
   /opt/factorio/bin/x64/factorio --create $SAVEFILE --map-gen-settings $MAPSETTINGS
 fi
+echo "starting server"
 /opt/factorio/bin/x64/factorio --start-server $SAVEFILE --server-settings $SERVERSETTINGS
